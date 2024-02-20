40680190e745fe295470cc7e0563027393c0b8dc
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-02-20 15:21:22 -0500

    chore: Init astro project

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['Geist', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				primary: '#242931'
+			}
+		},
+	},
+	plugins: [],
+}
